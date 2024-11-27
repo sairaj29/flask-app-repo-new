ddac5784fc939d01f382458c9c5581867ee9072a
sairaj <sairajincanada@gmai.com>
2024-11-27 12:26:53 -0500

    Update Flask app message

--- app.py
+++ app.py
@@ -3,12 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return 'Hello Cloud!'
-
-app.run(host='0.0.0.0', port=5000)
-@app.route('/')
-def hello_cloud():
     return 'Hello Cloud! Updated via Cloud Build'
 
-
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
