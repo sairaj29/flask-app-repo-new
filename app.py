a49303a97ce01f64c4597da1f361e16ef274efda
sairaj <sairajincanada@gmai.com>
2024-11-27 12:47:37 -0500

    Update Flask app message via Cloud Build

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return 'Hello Cloud! Updated via Cloud Build'
+    return 'Hello Cloud! Updated via Cloud Build Trigger'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+app.run(host='0.0.0.0', port=5000)
